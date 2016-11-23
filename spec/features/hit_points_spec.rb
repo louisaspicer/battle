e6e5b15a1c9574827f827089e1d6e29a21ccbb9f
Louisa <louisaspicer@gmail.com>
2016-11-23 15:08:18 +0000

    Added player 2 hit points

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,13 +1,15 @@
-require 'spec_helper'
+require 'spec_helper.rb'
 
 feature "Battle", :type => :feature do
-  scenario "Players fill in their names" do
+  scenario "Players see eachothers hit points" do
     visit "http://localhost:4567"
 
     fill_in "Player1", :with => "Tudor"
     fill_in "Player2", :with => "Louisa"
     click_button('Submit')
 
-    expect(page).to have_content("Player 1: Tudor, Player 2: Louisa")
+    expect(page).to have_content("Louisa: 100/100 HP")
+
   end
+
 end
