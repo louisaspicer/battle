72bacd414fc245de5ce6b3d6a629094f8e19d3fd
TudorTacal <tudortacal@gmail.com>
2016-11-23 14:32:38 +0000

    Challenge 19, enabled sessions

--- app.rb
+++ app.rb
@@ -1,18 +1,25 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
     p params
-    @player1 = params[:Player1]
-    @player2 = params[:Player2]
-    erb(:play)
+    session["Player1"] =   params[:Player1]
+    session["Player2"] =  params[:Player2]
+    redirect '/play'
   end
 
-
+  get '/play' do
+    p session
+    @player1 = session["Player1"]
+    @player2 = session["Player2"]
+    erb :play
+  end
 
   run! if app_file == $0
 end
